45439bd4e17b86290283e5b21bf70da1ed9389b3
gauravt17 <gauravtangri2010@gmail.com>
2022-09-24 05:19:05 -0400

    added code for retrying

--- src/test/java/com/crm/qa/testcases/LoginPageTest.java
+++ src/test/java/com/crm/qa/testcases/LoginPageTest.java
@@ -1,5 +1,6 @@
 package com.crm.qa.testcases;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -35,6 +36,7 @@ public class LoginPageTest extends TestBase{
 	public void logginInTest()
 	{
 			loginPage.signIn(prop.getProperty("username"), prop.getProperty("password"));
+			Assert.assertTrue(false);
 	}
 
 }
